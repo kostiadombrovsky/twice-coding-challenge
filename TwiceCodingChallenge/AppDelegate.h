f7d39326a285c5ae22c264f125a5c28f659e52e9
Kostia Dombrovsky <kdombrovsky@gmail.com>
2014-04-11 10:56:12 -0700

    Initial Commit

---
+++ TwiceCodingChallenge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TwiceCodingChallenge
+//
+//  Created by Kostia Dombrovsky on 11.4.14.
+//  Copyright (c) 2014 Kostia Dombrovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TwiceCodingChallenge/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TwiceCodingChallenge
+//
+//  Created by Kostia Dombrovsky on 11.4.14.
+//  Copyright (c) 2014 Kostia Dombrovsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
